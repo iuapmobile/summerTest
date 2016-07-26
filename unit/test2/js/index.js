2d9b0600502f9af7c68a8f83124cfdca38ffbed0
gct <gct@yonyou.com>
2016-07-26 23:51:20 +0800

    tt

---
+++ unit/test2/js/index.js
@@ -0,0 +1,21 @@
+﻿
+
+summerready = function(){
+return;
+    // here is your code...	
+    var y = $summer.offset($summer.byId('header')).h;
+    var width=$summer.offset(document.getElementsByTagName("body")[0]).w;		
+    var height = $summer.offset($summer.byId('main')).h;
+
+    summer.openFrame({
+        name: 'main',
+        url: 'html/main.html',
+        bounces: true,
+        rect: {
+            x: 0,
+            y: y,
+            w: width,
+            h: height
+        }
+    });
+}
