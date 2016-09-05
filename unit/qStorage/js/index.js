e4c6c3513c164b1fc675217ee2132a674bd5d13c
quhbc <quhbc@yonyou.com>
2016-09-05 16:31:04 +0800

    qq

---
+++ unit/qStorage/js/index.js
@@ -0,0 +1,24 @@
+﻿function setStorage(){
+	summer.setStorage('qq',123)
+
+}
+
+function getStorage(){
+	alert(summer.getStorage('qq'));
+}
+
+function setAppStorage(){
+	summer.setStorage('name',456)
+
+}
+
+function getAppStorage(){
+	alert(summer.getStorage('name'));
+}
+
+
+
+
+
+
+
