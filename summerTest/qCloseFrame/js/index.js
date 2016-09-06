2a20a75fd786e4c497628632780f66605eb5d5f6
quhbc <quhbc@yonyou.com>
2016-09-06 20:48:19 +0800

    qq

---
+++ summerTest/qCloseFrame/js/index.js
@@ -0,0 +1,32 @@
+﻿
+
+summerready = function(){
+    // here is your code...	
+    var y = $summer.offset($summer.byId('header')).h;
+    var width = $summer.offset(document.getElementsByTagName("body")[0]).w;		
+    var height = $summer.offset($summer.byId('main')).h;
+
+    summer.openFrame({
+        id: 'main1',
+        url: 'html/main.html',
+        bounces: true,
+        rect: {
+            x: 0,
+            y: y,
+            w: width,
+            h: height/2
+        }
+    });
+    
+     summer.openFrame({
+        id: 'main2',
+        url: 'html/main.html',
+        bounces: true,
+        rect: {
+            x: 0,
+            y: height/2,
+            w: width,
+            h: height/2
+        }
+    });
+}
---
+++ summerTest/qCloseFrame/js/main.js
@@ -0,0 +1,10 @@
+//here is your code...
+summerready = function () {
+	$summer.byId("content").innerHTML += "<h1 style='text-align: center'>Hello friends, welcome to touch the summer frame!</h1><h2 style='text-align: center'>The frame update at " +(new Date()).toLocaleString()+"</h2>";
+};
+
+function closeFrame(){
+	summer.closeFrame({
+            id: 'main2'
+     });
+}
