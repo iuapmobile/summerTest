8333a4dd2d58a9c453fca0371f64d9609cb83928
quhbc <quhbc@yonyou.com>
2016-09-12 13:50:44 +0800

    qq

--- xServiceTest/q$scanner/js/main.js
+++ xServiceTest/q$scanner/js/main.js
@@ -3,13 +3,23 @@ summerready = function () {
 	$summer.byId("content").innerHTML += "<h1 style='text-align: center'>Hello friends, welcome to touch the summer frame!</h1><h2 style='text-align: center'>The frame update at " +(new Date()).toLocaleString()+"</h2>";
 };
 
+function openScanner(){
+	$scanner.open({
+       callback : function (arges){        
+            $alert("二维码扫描结果:" + arges);
+	   }
+	 });	 
+}
+
+
 function openTest(){
 	 var twocodepath=$scanner.generateQRCode({
 		 size : 30,//二维码正方形的宽高
 		 content : "text"//生成二维码所需的源文字
 	});
 		
-	  $("img").attr("src",twocodepath);
+	  alert(twocodepath)
 
 }
+
 
