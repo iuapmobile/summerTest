98041d03675e1051b7c7f16310ee0582b0651f38
quhbc <quhbc@yonyou.com>
2016-09-14 13:27:21 +0800

    qq

--- xServiceTest/q$cache/js/main.js
+++ xServiceTest/q$cache/js/main.js
@@ -3,3 +3,11 @@ summerready = function () {
 	$summer.byId("content").innerHTML += "<h1 style='text-align: center'>Hello friends, welcome to touch the summer frame!</h1><h2 style='text-align: center'>The frame update at " +(new Date()).toLocaleString()+"</h2>";
 	    
 };
+function test(){
+	$cache.write('qq',123)
+}
+
+function test1(){
+	var qq=$cache.read('qq');
+	alert("缓存值为"+qq);
+}
