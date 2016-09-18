640068adc3419eb6aa54c50743af168a6a232b86
xingychb <xingychb@yonyou.com>
2016-09-18 09:21:33 +0800

    openFrameGroup用例

---
+++ summerTest/openFrameGroup/js/index.js
@@ -0,0 +1,51 @@
+﻿summerready = function(){
+    // here is your code...	
+    var y = $summer.offset($summer.byId('header')).h;
+    var width = $summer.offset(document.getElementsByTagName("body")[0]).w;		
+    var height = $summer.offset($summer.byId('main')).h;
+    summer.openFrameGroup({
+    	id: 'group1',    
+	    background: '#fff',    
+	    scrollEnabled: false,    
+	    position: {    
+	        top: 0,    
+	        left: 0,    
+	        width: width,    
+	        height: height    
+	    },    
+	    index: 0,    
+	    frames: [{    
+	        id: 'frame1',    
+	        url: 'html/frame1.html',    
+	        bgColor: '#fff',    
+	        hidden: true
+	    }, {    
+	        id: 'frame2',    
+	        url: 'html/frame2.html',    
+	        bgColor: '#fff',     
+	        hidden: false       
+	    }, {    
+	        id: 'frame3',    
+	        url: 'html/frame3.html',    
+	        bgColor: '#fff',    
+	        hidden: true    
+	    }, {    
+	        id: 'frame4',    
+	        url: 'html/frame4.html',    
+	        bgColor: '#fff',    
+	        hidden: true    
+	    }]    
+    },
+    function(){
+    	//success
+    },
+    function(){
+    	//error
+    });
+}
+function openGroup(index){
+	summer.setFrameGroupAttr({
+		id : "group1",
+		index : index
+	});
+}
