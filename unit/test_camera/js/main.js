847a96385449f554eccecbc8be0181dd5f02e952
gct <gct@yonyou.com>
2016-09-20 12:48:31 +0800

    tt

--- unit/test_camera/js/main.js
+++ unit/test_camera/js/main.js
@@ -1,7 +1,7 @@
 //here is your code...
 summerready = function () {
 	$summer.byId("content").innerHTML += "<h1 style='text-align: center'>Hello friends, welcome to touch the summer frame!</h1><h2 style='text-align: center'>The frame update at " +(new Date()).toLocaleString()+"</h2>";
-	return;
+	
 	//pulldown to refresh
     summer.setRefreshHeaderInfo({
         visible: true,
@@ -34,3 +34,56 @@ summerready = function () {
 	
 	
 };
+
+
+function open0() {
+	$camera.open({
+		callback : function xxxx(sender, args) {
+			alert( "typeof sender == " + typeof sender);
+			alert( "typeof args == " + typeof args);
+			alert(JSON.stringify(sender));
+			alert(JSON.stringify(args));
+			alert(sender.imgPath);
+			alert(args.imgPath);
+			$summer.byId("img0").src = args.imgPath;
+		}
+	});
+
+}
+
+
+function open1() {
+	$camera.open({
+		callback : "xxxx()"
+	})
+}
+
+function xxxx(sender, args) {
+	alert( "typeof sender == " + typeof sender);
+	alert( "typeof args == " + typeof args);
+	alert(JSON.stringify(sender));
+	alert(JSON.stringify(args));
+	alert(sender.imgPath);
+	alert(args.imgPath);
+	$summer.byId("img1").src = args.imgPath;
+}
+
+function open2() {
+	function xxxx2(sender, args) {
+		alert( "typeof sender == " + typeof sender);
+		alert( "typeof args == " + typeof args);
+		alert(JSON.stringify(sender));
+		alert(JSON.stringify(args));
+		alert(sender.imgPath);
+		alert(args.imgPath);
+		$summer.byId("img2").src = args.imgPath;
+	}
+
+	$camera.open({
+		callback : xxxx2
+	})
+}
+
+
+
+
