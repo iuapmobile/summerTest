d1f0c5437ec56fe26e2607c9b6f200505d320914
quhbc <quhbc@yonyou.com>
2016-09-23 09:58:50 +0800

    qq

--- xServiceTest/q$sqlite/js/main.js
+++ xServiceTest/q$sqlite/js/main.js
@@ -1,26 +1,27 @@
 function test0() {
-	var param = {
+	var param = {      //ios需要创建数据库，才能执行建表和插入数据等方法, android不需要创建数据库就可以执行建表与插入数据等方法。
 		db : 'qq.db'
 	}
-	$sqlite.openDB(param);
+	$sqlite.openDB(param);  //执行创建数据库语句，参数为json对象
 }
 function test1() {
 	var sql = "CREATE TABLE person (_id INTEGER PRIMARY KEY AUTOINCREMENT, name VARCHAR, xclass VARCHAR)";
-	var param = {
+	var param = {     
 		db : 'qq.db',
 		sql : sql
 	}
-	$sqlite.execSql(param);
+	$sqlite.execSql(param);   //sql为创建表结构语句，作为参数传入$sqlite.execSql()方法中执行   
 }
 	
 function test2() { 
 	var sql="INSERT INTO person (name, xclass) VALUES('屈海滨','石化')";
+	
 	 var param = {
 		db : 'qq.db',
 		sql : sql
 	}
 	for(var i=0;i<10;i++){  
-		 $sqlite.execSql(param);
+		 $sqlite.execSql(param); //sql为向表中插入数据语句，作为参数传入$sqlite.execSql()方法中执行
 	}	
 	 
 }
@@ -30,11 +31,10 @@ function test3() {
 	var param = {
 		"db" : 'qq.db',
 		"sql" : sql,
-		"pageIndex" : 0,
-		"pageSize" : 10
+		"pageIndex" : 0,    //页号，从0开始
+		"pageSize" : 10		//每页的记录数，从1开始
 	}
-	//var list=$sqlite.query(param);
-	var list = $sqlite.queryByPage(param);
+	var list = $sqlite.queryByPage(param); //sql为查询语句，作为参数传入$sqlite.queryByPage()方法中执行
 	alert(list);
 
 }
@@ -43,7 +43,7 @@ function test4() {
 	var param = {
 		db : 'qq.db'
 	}
-	var list = $sqlite.exist(param)
+	var list = $sqlite.exist(param)    //$sqlite.exist()判断数据库是否已经创建存在  android在执行其他sq语句时自动建立数据库  ios必须先要建立数据库
 	alert(list)
 }
 
@@ -57,7 +57,7 @@ function test5() {
            startIndex : 3, //起始记录索引号(含)
            endIndex : 5         //结束记录索引号(含)
 	  }
-  	var data = $sqlite.query(param);
+  	var data = $sqlite.query(param);  //用来查询数据库
   	alert(data);
 }
 
