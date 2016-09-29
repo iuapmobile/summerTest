9caa7771ee17ec91ff960cc6bf172a28f9a6c239
wuxlr <wuxlr@yonyou.com>
2016-09-29 15:51:35 +0800

    提交cordova测试用例

---
+++ cordovaTest/cordovaCall/js/index.js
@@ -0,0 +1,22 @@
+﻿summerready = function() {
+		openFrame('codorva调用','codorva','html/codorva.html')
+}
+function openFrame(title,name, url) {
+
+	var y = $summer.offset($summer.byId('header')).h;
+	var width = $summer.offset(document.getElementsByTagName("body")[0]).w;
+	var height = $summer.offset($summer.byId('main')).h;
+	$('h3').html(title);
+	summer.openFrame({
+		name : name,
+		url : url,
+		bounces : true,
+		rect : {
+			x : 0,
+			y : y,
+			w : width,
+			h : height
+		}
+	});
+
+}
