b60192e5a6da2546ba53a0c6dcd56ea6ac484d2f
xingychb <xingychb@yonyou.com>
2016-10-26 15:35:26 +0800

    ·1

--- xServiceTest/qNet/js/new.js
+++ xServiceTest/qNet/js/new.js
@@ -2,12 +2,17 @@
  * Created by Administrator on 2016/10/17.
  */
 summerready = function () {
+	$summer.alert(summer.UMNet. getNetworkInfo()) 
 };
 function test1(){
     alert(summer.UMNet.available())  //返回值为boolean类型         true||false  ios返回值不正确
 }
 
 function test2(){
-    $summer.alert(summer.UMNet. getNetworkInfo())  //android返回值为json类型的 string key值为Type
-
+     //android返回值为json类型的 string key值为Type
+	summer.openWin({
+        "id" : 'a',
+        "url" : 'html/a.html'
+        
+    });
 }
