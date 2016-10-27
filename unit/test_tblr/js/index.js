820cc95735642f8d69f0453013a51918bd93eb48
gct <gct@yonyou.com>
2016-10-27 14:15:39 +0800

    tt

---
+++ unit/test_tblr/js/index.js
@@ -0,0 +1,22 @@
+﻿
+
+summerready = function(){
+    // here is your code...	
+    var y = $summer.offset($summer.byId('header')).h;
+    //var width = $summer.offset(document.getElementsByTagName("body")[0]).w;		
+    var height = $summer.offset($summer.byId('main')).h;
+    
+    var bottom = $summer.offset($summer.byId('footer')).h;
+
+    summer.openFrame({
+        id: 'main',
+        url: 'html/main.html',
+        bounces: true,
+        position: {
+            left: 0,
+            right:0,
+            top: y,
+            bottom:bottom
+        }
+    });
+}
