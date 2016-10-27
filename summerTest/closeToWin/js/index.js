d4192af1e5a757848e3827c6366fd3dd49aad1de
quhbc <quhbc@yonyou.com>
2016-10-27 16:16:29 +0800

    qq

---
+++ summerTest/closeToWin/js/index.js
@@ -0,0 +1,20 @@
+﻿
+
+summerready = function(){
+    // here is your code...	
+    var y = $summer.offset($summer.byId('header')).h;
+    //var width = $summer.offset(document.getElementsByTagName("body")[0]).w;		
+    //var height = $summer.offset($summer.byId('main')).h;
+
+    summer.openFrame({
+        id: 'main',
+        url: 'html/main.html',
+        bounces: true,
+        position: {
+            left: 0,
+            top: y,
+            width: 'auto',
+            height: 'auto'
+        }
+    });
+}
