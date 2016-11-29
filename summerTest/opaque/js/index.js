2860381c53112cf5b5065095e7cff68d0e4def2d
quhbc <quhbc@yonyou.com>
2016-11-29 16:29:15 +0800

    qq

---
+++ summerTest/opaque/js/index.js
@@ -0,0 +1,29 @@
+﻿
+
+summerready = function(){
+    // here is your code...	
+    var height= $summer.offset($summer.byId('win')).h;
+   summer.openFrame({
+        id: 'main',
+        url: 'html/main.html',
+        bounces: true,
+        position: {
+            top: 0,
+			bottom:0,
+            left: 0,
+            right: 0
+        }
+    });
+    summer.openFrame({
+        id: 'header',
+        url: 'html/header.html',
+        bounces: true,
+       	opaque:false,
+        position: {
+            top:0,
+			bottom: height-44,
+            left: 0,
+            right: 0
+        }
+    });
+}
