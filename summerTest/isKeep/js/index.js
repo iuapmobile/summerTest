32faac2049cee37dd5f8eda414af20d40ea6f47f
quhbc <quhbc@yonyou.com>
2016-12-02 09:46:38 +0800

    qq

---
+++ summerTest/isKeep/js/index.js
@@ -0,0 +1,17 @@
+﻿
+
+summerready = function(){
+
+    document.getElementById('qq').innerHTML= summer.pageParam.name;
+   
+}
+
+function openWin(){
+	summer.openWin({
+	    url:'index.html',
+	    pageParam: {
+	        name: 'isKeep'
+	    },
+	    isKeep:false
+	});
+}
