d30a499e0eb98c7e3eb0daf10c07988298e3b531
zhouleim <zhouleim@yonyou.com>
2018-04-17 15:21:30 +0800

    更新微信分享demo

---
+++ cordovaTest/justoask/js/index.js
@@ -0,0 +1,41 @@
+﻿
+
+summerready = function(){
+    // here is your  
+}
+
+function wechat() {
+   // var name = summer.getStorage("userinfo").name;
+    Wechat.isInstalled(function (installed) {
+    	summer.toast({
+             "msg" : "微信已经安装" 
+        })
+        // 朋友圈和好友
+        var scene = Wechat.Scene.SESSION;
+        // 标题
+        var title = "张三邀请您加入" ;
+        // 描述
+        var description = "Moli让企业智能应用，触手可及 ！";
+        // 跳转地址
+        var webpageUrl = "www.baidu.com";
+        var params = {
+            scene: scene,
+            message: {
+                title: title,
+                description: description,
+                media: {
+                    type: Wechat.Type.WEBPAGE,
+                    webpageUrl: webpageUrl
+                }
+            }
+        };
+        Wechat.share(params, function () {
+            summer.toast({"msg": "分享成功"});
+        }, function (reason) {
+            summer.toast({"msg": reason});
+        });
+    }, function (reason) {
+        summer.toast({"msg": reason});
+    });
+    //closeMark();
+}
